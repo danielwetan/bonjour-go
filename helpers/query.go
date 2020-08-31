8a4c9d9e55e7bedc738d0c71cbda0adfefb8607b
Daniel Saputra <danielwetan.io@gmail.com>
2020-09-01 03:24:16 +0700

    feature: Display User info

--- helpers/query.go
+++ helpers/query.go
@@ -4,4 +4,6 @@ var Query = map[string]string{
 	"login":      "SELECT * FROM users WHERE email = ?",
 	"register":   "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
 	"getContact": "SELECT id, name, email, profile_img, about FROM users WHERE NOT id=? ORDER BY name DESC LIMIT 10",
+	"getUser":    "SELECT id, name, email, profile_img, about FROM users WHERE id=?",
+	"updateUser": "UPDATE users SET name=?, email=? WHERE id = ?",
 }
