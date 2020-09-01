87a177e71d63c6bedd2ab028ea42bebdf77b1674
Daniel Saputra <danielwetan.io@gmail.com>
2020-09-01 14:15:26 +0700

    fix: Improve design pattern and adding error handling on Display Messages

--- controllers/message.go
+++ controllers/message.go
@@ -27,54 +27,61 @@ func Message(w http.ResponseWriter, r *http.Request) {
 		// Scan(&data.ID, &data.ReceiverID, &data.SenderiID, &data.SenderName, &data.ProfileImg, &data.Message, &data.CreatedAt)
 
 		// Latest message
-		if id != nil {
-			rows, err := db.Query(helpers.Query["latestMessages"], id[0])
-			if err != nil {
-				panic(err)
-			}
-			var result []interface{}
-			defer rows.Close()
-			for rows.Next() {
-				data := models.LatestMessages{}
-				err = rows.Scan(&data.ID, &data.ReceiverID, &data.SenderiID, &data.SenderName, &data.ProfileImg, &data.Message, &data.CreatedAt)
-				result = append(result, data)
+		switch {
+		case id != nil:
+			{
+				rows, err := db.Query(helpers.Query["latestMessages"], id[0])
 				if err != nil {
 					panic(err)
 				}
-			}
-			err = rows.Err()
-			if err != nil {
-				panic(err)
-			}
-
-			res := helpers.ResponseMsg(true, result)
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(res)
-		}
-
-		// Conversation
-		if sender != nil && receiver != nil {
-			fmt.Println("ACTIVE")
-			rows, err := db.Query(helpers.Query["conversation"], sender[0], receiver[0], sender[0], receiver[0])
-			if err != nil {
-				panic(err)
-			}
-			var result []interface{}
-			defer rows.Close()
-			for rows.Next() {
-				data := models.Conversation{}
-				err = rows.Scan(&data.ID, &data.SenderID, &data.ReceiverID, &data.Message, &data.CreatedAt)
-				result = append(result, data)
+				var result []interface{}
+				defer rows.Close()
+				for rows.Next() {
+					data := models.LatestMessages{}
+					err = rows.Scan(&data.ID, &data.ReceiverID, &data.SenderiID, &data.SenderName, &data.ProfileImg, &data.Message, &data.CreatedAt)
+					result = append(result, data)
+					if err != nil {
+						panic(err)
+					}
+				}
+				err = rows.Err()
 				if err != nil {
 					panic(err)
 				}
-			}
-			err = rows.Err()
-			if err != nil {
-				panic(err)
-			}
 
-			res := helpers.ResponseMsg(true, result)
+				res := helpers.ResponseMsg(true, result)
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(res)
+			}
+		case sender != nil && receiver != nil:
+			{
+				// Conversation
+				rows, err := db.Query(helpers.Query["conversation"], sender[0], receiver[0], sender[0], receiver[0])
+				if err != nil {
+					panic(err)
+				}
+				var result []interface{}
+				defer rows.Close()
+				for rows.Next() {
+					data := models.Conversation{}
+					err = rows.Scan(&data.ID, &data.SenderID, &data.ReceiverID, &data.Message, &data.CreatedAt)
+					result = append(result, data)
+					if err != nil {
+						panic(err)
+					}
+				}
+				err = rows.Err()
+				if err != nil {
+					panic(err)
+				}
+
+				res := helpers.ResponseMsg(true, result)
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(res)
+			}
+		default:
+			body := "Invalid URL"
+			res := helpers.ResponseMsg(false, body)
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(res)
 		}
